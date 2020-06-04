054e8cd3a8a3ff4d11ad8a2d81983db53ede66c1
Agung Wicaksana <agung.wicaksaana@gmail.com>
2020-06-04 09:40:00 +0700

    Selesai setup materialize jquery dan style.css

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
 import "regenerator-runtime";
+import "materialize-css/dist/css/materialize.min.css";
+import "materialize-css/dist/js/materialize.min.js";
+import "./styles/style.css"
+
+import main from "./script/view/main.js";
+
+document.addEventListener("DOMContentLoaded", main);
